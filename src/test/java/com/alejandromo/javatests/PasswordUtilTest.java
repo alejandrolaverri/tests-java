2ca804425ebfc01c9d235ab42ae4d3d51c292de6
Alejandro Moreno Olaverri <alejandro.moreno.olaverri@gmail.com>
2023-02-24 10:32:40 +0100

    Implementada y testada clase PasswordUtil

---
+++ src/test/java/com/alejandromo/javatests/PasswordUtilTest.java
@@ -0,0 +1,26 @@
+package com.alejandromo.javatests;
+
+import org.junit.Test;
+
+import static com.alejandromo.javatests.PasswordUtil.SecurityLevel.*;
+import static org.junit.Assert.*;
+
+public class PasswordUtilTest {
+
+    @Test
+    public void weak_when_has_less_than_8_letters() {
+        assertEquals(WEAK, PasswordUtil.assessPassword("123aa!"));
+    }
+    @Test
+    public void weak_when_has_only_letters() {
+        assertEquals(WEAK, PasswordUtil.assessPassword("abcdefghjk"));
+    }
+    @Test
+    public void medium_when_has_letters_and_numbers() {
+        assertEquals(MEDIUM, PasswordUtil.assessPassword("abcd12345"));
+    }
+    @Test
+    public void strong_when_has_letters_numbers_and_symbols() {
+        assertEquals(STRONG, PasswordUtil.assessPassword("abcd123!"));
+    }
+}
